7a2b90febd4c431fea25a3d6b5d6b1a60c8e26c8
Christopher Santos <crsantos@nuskin.com>
2023-07-02 04:11:59 +0800

    fix(service): remove hardcoded url

--- src/service.ts
+++ src/service.ts
@@ -3,7 +3,9 @@ interface Photo {
 }
 
 export async function fetchPhotosByTags(tags: string[]): Promise<Photo[]> {
-	const baseURL = "http://localhost:8888/.netlify/functions/search";
+	const baseURL = `${
+		import.meta.env.VITE_NETLIFY_FUNCTION_BASE_URL
+	}/.netlify/functions/search`;
 	const url = `${baseURL}?tags=${tags.join(",")}`;
 	const res = await fetch(url);
 	const { data } = await res.json();
